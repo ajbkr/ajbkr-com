c2badad0c65f02831137ed2fc81a01fca4b74566
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-01-12 17:28:38 +0000

    Update TODO, ignore Vim swap files, and remove /users route

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var logger       = require('morgan');
 var path         = require('path');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -28,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
