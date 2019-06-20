b2256a0f9f1579b8df6ef7b88bab6fb2ce9f265a
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-06-20 11:35:58 +0100

    Move quotations into separate JSON file

--- routes/index.js
+++ routes/index.js
@@ -3,101 +3,88 @@ const router = express.Router()
 
 const getNavigation = require('../helpers/get-navigation')
 
+const quotations = require('./quotations.json')
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  const quotation = quotations[req.url]
+
   res.render('index', {
     gravatarImageUrl: req.app.locals.gravatarImageUrl,
     navigation: getNavigation(req.url),
     production: res.app.get('env') === 'production',
-    quotation: {
-      attribution: 'J. R. R. Tolkien, The Fellowship of the Ring',
-      text: 'Do not meddle in the affairs of wizards, for they are subtle ' +
-       'and quick to anger.'
-    }
+    quotation
   })
 })
 
 router.get('/finished', function (req, res, next) {
+  const quotation = quotations[req.url]
+
   res.render('finished', {
     gravatarImageUrl: req.app.locals.gravatarImageUrl,
     navigation: getNavigation(req.url),
     production: res.app.get('env') === 'production',
-    quotation: {
-      attribution: 'Mark Twain',
-      text: 'Giving up smoking is the easiest thing in the world. I know ' +
-       "because I've done it thousands of times."
-    },
+    quotation,
     title: 'Finished'
   })
 })
 
 router.get('/linkedout', function (req, res, next) {
+  const quotation = quotations[req.url]
+
   res.render('linkedout', {
     gravatarImageUrl: req.app.locals.gravatarImageUrl,
     navigation: getNavigation(req.url),
     production: res.app.get('env') === 'production',
-    quotation: {
-      attribution: 'Buckminster Fuller',
-      text: 'We keep inventing jobs because of this false idea that ' +
-       'everybody has to be employed at some kind of drudgery because, ' +
-       'according to Malthusian-Darwinian theory he must justify his right ' +
-       'to exist.'
-    },
+    quotation,
     title: 'LinkedOut'
   })
 })
 
 router.get('/personal-development', function (req, res, next) {
+  const quotation = quotations[req.url]
+
   res.render('personal-development', {
     gravatarImageUrl: req.app.locals.gravatarImageUrl,
     navigation: getNavigation(req.url),
     production: res.app.get('env') === 'production',
-    quotation: {
-      attribution: 'Chuck Palahniuk, Fight Club',
-      text: 'I say never be complete, I say stop being perfect, I say ' +
-       "let... let's evolve, let the chips fall where they may."
-    },
+    quotation,
     title: 'Personal Development'
   })
 })
 
 router.get('/projects', function (req, res, next) {
+  const quotation = quotations[req.url]
+
   res.render('projects/index', {
     gravatarImageUrl: req.app.locals.gravatarImageUrl,
     navigation: getNavigation(req.url),
     production: res.app.get('env') === 'production',
-    quotation: {
-      attribution: 'Time, Alice Through the Looking Glass',
-      text: 'Everyone parts with everything eventually, my dear.'
-    },
+    quotation,
     title: 'Projects'
   })
 })
 
 router.get('/projects/electronics', function (req, res, next) {
+  const quotation = quotations[req.url]
+
   res.render('projects/electronics', {
     gravatarImageUrl: req.app.locals.gravatarImageUrl,
     navigation: getNavigation(req.url),
     production: res.app.get('env') === 'production',
-    quotation: {
-      attribution: 'Nietzsche',
-      text: 'He who fights with monsters should be careful lest he thereby ' +
-       'become a monster'
-    },
+    quotation,
     title: 'Electronics - Projects'
   })
 })
 
 router.get('/transformation', function (req, res, next) {
+  const quotation = quotations[req.url]
+
   res.render('transformation', {
     gravatarImageUrl: req.app.locals.gravatarImageUrl,
     navigation: getNavigation(req.url),
     production: res.app.get('env') === 'production',
-    quotation: {
-      attribution: 'Heraclitus',
-      text: 'You could not step twice into the same river; everything ' +
-       'changes and nothing stands still.'
-    },
+    quotation,
     title: 'Transformation'
   })
 })
